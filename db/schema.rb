70287ddf4b48a3b00e93aac401cbcfe06bb6bac6
Pedro Manoel <dev.pedromanoel@pm.me>
2023-06-26 21:13:03 -0300

    Create UserPassword model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_27_000725) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_27_001229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_000725) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_passwords", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "password_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["password_id"], name: "index_user_passwords_on_password_id"
+    t.index ["user_id"], name: "index_user_passwords_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_27_000725) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_passwords", "passwords"
+  add_foreign_key "user_passwords", "users"
 end
