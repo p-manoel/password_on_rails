bc6bf92097c2332102588c54f6a03082c0a20aca
Pedro Manoel <dev.pedromanoel@pm.me>
2023-06-28 23:49:10 -0300

    Add routes, controller and views to Password resource

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,32 @@
+class PasswordsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_password, except: [:index, :new, :create]
+
+    def index
+        @passwords = current_user.passwords
+    end
+
+    def new
+        @password = Password.new
+    end
+
+    def create
+        @password = current_user.passwords.create(password_params)
+
+        if @password.persisted?
+            redirect_to @password, notice: "Password created!"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:password).permit(:url, :username, :password)
+    end
+
+    def set_password
+        @password = current_user.passwords.find(params[:id])
+    end
+end
